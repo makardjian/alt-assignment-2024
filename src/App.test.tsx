927d94535f9fad3f0a77e0809e9f371fd97d4f35
Michael Kardjian <michaelkardjian@Michaels-MacBook-Pro.local>
2024-08-07 17:47:58 -0700

    stabalize dependencies to overcome import error on fireEvent and screen

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search bar and results', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const searchInput = screen.getByPlaceholderText(/search movies/i);
+  fireEvent.change(searchInput, { target: { value: 'Inception' } });
+
+  const searchButton = screen.getByText(/search/i);
+  fireEvent.click(searchButton);
+
+  const resultItem = await screen.findByText(/Inception/i);
+  expect(resultItem).toBeInTheDocument();
 });
